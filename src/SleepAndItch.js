8da6433a72a9ee81dab38072cdd4433c57c7d00f
Karolina Chodera <karolinachodera@gmail.com>
2023-05-22 11:43:08 +0200

    add sliders for sleep and itch

--- src/SleepAndItch.js
+++ src/SleepAndItch.js
@@ -1,3 +1,44 @@
-function SleepAndItch() {}
+function SleepAndItch({ handleChange, points }) {
+  const rangeData = [
+    [
+      "itch",
+      "Nasielenie świądu w skali od 0 (brak świądu) do 10 (mocno nasilony)",
+    ],
+    [
+      "sleep",
+      "Zaburzenia snu w skali od 0 (brak zaburzeń snu) do 10 (całkowita bezsenność)",
+    ],
+  ];
+
+  function Slider({ symptom, description, fieldsetIndex }) {
+    return (
+      <fieldset>
+        <label htmlFor={symptom}>{description}</label>
+        <input
+          id={symptom}
+          type="range"
+          min="0"
+          max="10"
+          name={symptom}
+          value={points[fieldsetIndex]}
+          onChange={(e) => handleChange(e, fieldsetIndex)}
+        ></input>
+        <output htmlFor={symptom}>{points[fieldsetIndex]}</output>
+      </fieldset>
+    );
+  }
+  const sliders = rangeData.map((symptom, index) => {
+    return (
+      <Slider
+        symptom={symptom[0]}
+        description={symptom[1]}
+        key={symptom}
+        handleChange={handleChange}
+        fieldsetIndex={index}
+      />
+    );
+  });
+  return sliders;
+}
 
 export default SleepAndItch;
