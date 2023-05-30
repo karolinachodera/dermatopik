b7e683c648552f0474e747c872b879fd3c995dfe
Karolina Chodera <karolinachodera@gmail.com>
2023-05-30 12:51:00 +0200

    use a new variable to avoid mutating argument

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -1,15 +1,15 @@
 function Areas({ parts, side, points, handleChange }) {
   const areas = parts.map((part, index) => {
-    index = side === "back" ? index + 7 : index;
-    let isChecked = points[index] === 0 ? "false" : "true";
+    let id = side === "back" ? index + 7 : index;
+    let isChecked = points[id] === 0 ? "false" : "true";
     return (
       <div
-        key={`part-${index}`}
+        key={`part-${id}`}
         className={`body-part ${part[0]} ${
           isChecked === "true" ? "checked" : ""
         }`}
         data-value={isChecked === "false" ? part[1] : 0}
-        onClick={(e) => handleChange(e, index)}
+        onClick={(e) => handleChange(e, id)}
       ></div>
     );
   });
