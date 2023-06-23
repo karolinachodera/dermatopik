965968111f64a5db5fe0427a7bde59b75554e9e8
Karolina Chodera <karolinachodera@gmail.com>
2023-06-23 13:12:31 +0200

    add List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,17 @@
-export function List({ elements }) {
-  const list = elements.map((element) => {
+export function List({ elements, section }) {
+  const list = elements.map((element, index) => {
     let checkboxes = [];
     if (element.frequency) {
       for (let i = 0; i < element.frequency; i++) {
-        checkboxes.push(<input type="checkbox" />);
+        checkboxes.push(<input key={`${section}-${i}`} type="checkbox" />);
       }
     }
+    if (section === "events") {
+      checkboxes.push(<input key={`${section}-1`} type="checkbox" />);
+    }
     return (
-      <li>
-        {element.name || element.event || element}
+      <li key={`${section}-${index}`}>
+        {element.name || element}
         {checkboxes}
       </li>
     );
