161728ddc63ded173699a4bfada3f43a932d32bf
karolinachodera <karolinachodera@gmail.com>
2023-07-05 11:22:32 +0200

    Dashboard (#4)
    * start dashboard
    
    * add AddingForm component
    
    * check input section (WIP)
    
    * change node version to 18.16
    
    * change inputs data structure and form generating
    
    * save inputs data only after submitting
    
    * add List component
    
    * simplify submitting handlers
    
    * change values to camelCase
    
    * add mock data for lists
    
    * add List component
    
    * remove creating form in Section component
    
    * add separate form components for every section
    
    * add finishing Scorad evaluation on Dashboard
    
    * add required attribute to textarea
    
    * add button to remove list items
    
    * change let to const
    
    * add result description to scoradResult state

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,17 +1,8 @@
 function Result({ scoradResult }) {
-  let description;
-  if (scoradResult <= 14) {
-    description = "Twoje AZS ma łagodny przebieg";
-  } else if (scoradResult > 14 && scoradResult <= 39) {
-    description = "Twoje AZS ma umiarkowany przebieg";
-  } else {
-    description = "Twoje AZS ma ciężki przebieg";
-  }
-
   return (
-    <section>
-      <h2>Wynik SCORAD: {scoradResult}</h2>
-      <p>{description}</p>
+    <section className="result">
+      <h2>Wynik SCORAD: {scoradResult.result}</h2>
+      <p>{scoradResult.description}</p>
     </section>
   );
 }
