ee64686fde593b6d66984824e9f845e440dacc46
Karolina Chodera <karolinachodera@gmail.com>
2023-08-09 13:03:23 +0200

    add types to index, App and Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 
 import Scorad from "../Scorad/Scorad";
 import Button from "../Button/Button";
@@ -25,89 +25,89 @@ import {
 } from "../../constants/dashboardInputs";
 
 function Dashboard() {
-  const [todayScorad, setTodayScorad] = useState(null);
+  const [todayScorad, setTodayScorad] = useState < {result: number, description: string} | null>(null);
   const [displayForm, setDisplayForm] = useState(false);
   const [drugs, setDrugs] = useState(drugsMock);
   const [cares, setCares] = useState(caresMock);
   const [events, setEvents] = useState(eventsMock);
   const [notes, setNotes] = useState(notesMock);
 
-  function handleButtonClick(e) {
-    if (e.target.name === "scorad") {
+  function handleButtonClick(e: Event) {
+    if ((e.target as HTMLButtonElement).name === "scorad") {
       setDisplayForm(true);
     }
   }
 
-  function handleDrugAdding(e, newDrug) {
+  function handleDrugAdding(e: Event, newDrug: {name: string, frequency: number}) {
     e.preventDefault();
     setDrugs([...drugs, newDrug]);
-    e.target.reset();
+    (e.target as HTMLFormElement).reset();
   }
 
-  function handleCareAdding(e, newCare) {
+  function handleCareAdding(e: Event, newCare: {name: string, frequency: number}) {
     e.preventDefault();
     setCares([...cares, newCare]);
-    e.target.reset();
+    (e.target as HTMLFormElement).reset();
   }
 
-  function handleEventAdding(e, newEvent) {
+  function handleEventAdding(e: Event, newEvent: string) {
     e.preventDefault();
     setEvents([...events, newEvent]);
-    e.target.reset();
+    (e.target as HTMLFormElement).reset();
   }
 
-  function handleNoteAdding(e, newNote) {
+  function handleNoteAdding(e: Event, newNote: string) {
     e.preventDefault();
     setNotes([...notes, newNote]);
-    e.target.reset();
+    (e.target as HTMLFormElement).reset();
   }
 
-  function handleScoradFinish(scoradResult) {
+  function handleScoradFinish(scoradResult: {result: number, description: string}) {
     setTodayScorad(scoradResult);
     setDisplayForm(false);
   }
 
-  function handleRemoveDrug(index) {
+  function handleRemoveDrug(index: number) {
     const newDrugs = [...drugs];
     newDrugs.splice(index, 1);
     setDrugs(newDrugs);
   }
 
-  function handleRemoveCare(index) {
+  function handleRemoveCare(index: number) {
     const newCares = [...cares];
     newCares.splice(index, 1);
     setCares(newCares);
   }
 
-  function handleRemoveEvent(index) {
+  function handleRemoveEvent(index: number) {
     const newEvents = [...events];
     newEvents.splice(index, 1);
     setEvents(newEvents);
   }
 
-  function handleRemoveNote(index) {
+  function handleRemoveNote(index: number) {
     const newNotes = [...notes];
     newNotes.splice(index, 1);
     setNotes(newNotes);
   }
 
-  function ScoradSection() {
-    if (todayScorad === null && displayForm === false) {
-      return (
-        <Button
-          description="Oceń SCORAD"
-          handleClick={handleButtonClick}
-          buttonName="scorad"
-        />
-      );
-    } else if (displayForm === true) {
+  function ScoradSection(): ReactElement {
+    if (displayForm === true) {
       return <Scorad handleScoradFinish={handleScoradFinish} />;
-    } else {
+    } else if (todayScorad) {
       return (
         <p>
           Twój dzisiejszy wynik SCORAD to {todayScorad.result} punktów.{" "}
           {todayScorad.description}.
         </p>
+      );
+    } else {
+            return (
+        <Button
+          description="Oceń SCORAD"
+          handleClick={handleButtonClick}
+          buttonName="scorad"
+        />
       );
     }
   }
