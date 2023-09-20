677e5c6f8897e3013b4ad5dd068613bd76d6cfed
Karolina Chodera <karolinachodera@gmail.com>
2023-09-20 14:44:54 +0200

    remove redundant code

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,14 +3,10 @@ import { Navigate, Link, useLocation } from 'react-router-dom';
 import { db, usersRef, getUserScoradResults, setUserScoradResults } from "../../config/firebase";
 import { useRootContext } from '../Root/RootContext';
 
-import Scorad from "../Scorad/Scorad";
-import Button from "../Button/Button";
 import Section from "../Section/Section";
 import { NotesForm } from "../NotesForm/NotesForm";
 import { List } from "../List/List";
 import { LineChart } from "../LineChart/LineChart";
-
-import "./Dashboard.scss";
 
 import {
   notesTextarea,
@@ -29,9 +25,6 @@ interface ScoradResult {
 }
 
 function Dashboard(): ReactElement {
-  // const [todayScorad, setTodayScorad] = useState<ScoradResult | null>(null);
-  // const [scoradList, setScoradList] = useState<ScoradResult[]>([]);
-  // const [displayForm, setDisplayForm] = useState<boolean>(false);
   const { scoradList, setScoradList, todayScorad, setTodayScorad } = useRootContext();
   const [notes, setNotes] = useState<string[]>(notesMock);
 
@@ -62,22 +55,14 @@ function Dashboard(): ReactElement {
   }
 
   function isTodayScorad(scoradResult: ScoradResult): boolean {
-    console.log("check");
-    console.log(scoradList.length);
-    console.log(scoradList);
     if (scoradList.length > 0) {
-      console.log(scoradList);
       const lastDate: Date = scoradList[scoradList.length - 1].date instanceof Date ? (scoradList[scoradList.length - 1].date) : (scoradList[scoradList.length - 1].date).toDate();
       const resultDate: Date = scoradResult.date;
-      console.log(resultDate);
-      console.log(lastDate);
       const isSameDate: boolean =
         resultDate.getDate() === lastDate.getDate() &&
         resultDate.getMonth() === lastDate.getMonth() &&
         resultDate.getFullYear() === lastDate.getFullYear();
-      console.log("isDameDate" + isSameDate)
       return isSameDate;
-
     } else {
       return false;
     }
@@ -89,32 +74,15 @@ function Dashboard(): ReactElement {
     setNotes(newNotes);
   }
 
-  function ScoradSection(): ReactElement {
-  if (todayScorad) {
-      return (
-        <>
-        <p>
-          Twój dzisiejszy wynik SCORAD to {todayScorad.result} punktów.{" "}
-          {todayScorad.description}.
-          </p>
-          <LineChart chartData={scoradList} />
-          <Link to="/scorad">Oceń Scorad</Link>
-          </>
-      );
-    } else {
-      return (
-              <>
-          <LineChart chartData={scoradList} />
-          <Link to="/scorad">Oceń Scorad</Link>
-          </>
-      );
-    }
-  }
-
   return (
     <main>
       <Section width="full-width" header="SCORAD" id="scorad">
-        <ScoradSection />
+        {todayScorad &&
+          <p>
+            Twój dzisiejszy wynik SCORAD to {todayScorad.result} punktów.{" "}
+            {todayScorad.description}.
+          </p>}
+          <LineChart chartData={scoradList} />
       </Section>
       <Section header="Notatki" id="notes" width="half-width">
         <List
@@ -123,6 +91,11 @@ function Dashboard(): ReactElement {
           handleRemoveItem={handleRemoveNote}
         />
         <NotesForm handleSubmit={handleNoteAdding} textarea={notesTextarea} />
+      </Section>
+      <Section id="navigation" width="full-width">
+        <Link to="/scorad" className="button">Oceń Scorad</Link>
+        <Link to="/daily" className="button">Daily Check</Link>
+        <Link to="/notes" className="button">Notatki</Link>
       </Section>
     </main>
   );
