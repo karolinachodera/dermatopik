98d1f89279ad1c55ae615ff2e42104a0eddc005a
Karolina Chodera <karolinachodera@gmail.com>
2023-09-20 14:45:26 +0200

    add SectionProps and set header as optional

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,10 +1,19 @@
 import { ReactElement, ReactNode } from "react";
 import "./Section.scss";
 
-function Section({ children, header, width, id }: { children: ReactNode, header: string, width: string, id: string}): ReactElement {
+interface SectionProps {
+  children: ReactNode,
+  header?: string,
+  width?: string,
+  id: string,
+}
+
+function Section({ children, header, width, id }: SectionProps): ReactElement {
   return (
-    <section className={`${width} main`} id={id}>
-      <h2>{header}</h2>
+    <section className={width} id={id}>
+      {header &&
+        <h2>{header}</h2>
+       }
       {children}
     </section>
   );
