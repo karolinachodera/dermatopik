81157214816b9aef9b021b1fd88f82159572751a
Karolina Chodera <karolinachodera@gmail.com>
2023-09-21 13:07:42 +0200

    add responsiveness to navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Nav.scss";
+import { ReactElement } from "react";
 
-export function Nav() {
+export function Nav(): ReactElement {
+    function handleIconClick(e: any) {
+        const nav: HTMLElement = document.getElementById("mainNav")!;
+        nav.classList.toggle("open");
+    }
+
+    function handleLinkClick() {
+        const nav: HTMLElement = document.getElementById("mainNav")!;
+        if (nav.classList.contains("open")) {
+            nav.classList.remove("open");
+        }
+    }
+
     return (
-        <nav>
-            <Link to="/">Strona główna</Link>
-            <Link to="/scorad">Oceń Scorad</Link>
-            <Link to="/daily">Daily check</Link>
-            <Link to="/notes">Notatki</Link>
+        <nav id="mainNav">
+            <NavLink to="/" onClick={handleLinkClick}>Strona główna</NavLink>
+            <NavLink to="/scorad" onClick={handleLinkClick}>Oceń Scorad</NavLink>
+            <NavLink to="/daily" onClick={handleLinkClick}>Daily check</NavLink>
+            <NavLink to="/notes" onClick={handleLinkClick}>Notatki</NavLink>
+            <NavLink to="/login" onClick={handleLinkClick}>Logowanie</NavLink>
+            <span className="navIcon" onClick={handleIconClick}>=</span>
         </nav>
     )
 }
